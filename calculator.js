893445fdb7e8cde4fbed99026092a569f7c9b9ae
Abdulaziz-alansari <aziz83alansari@gmail.com>
2021-02-03 15:52:57 +0300

    done

---
+++ calculator.js
@@ -0,0 +1,13 @@
+let x =5;
+let y =10;
+let str ="+";
+
+if ( str === "+") {
+
+    console.log(x + y);
+
+}
+else {
+    console.log("false");
+
+}
---
+++ story.js
@@ -0,0 +1,6 @@
+let name =  "Mishary";
+let age = "25";
+let hobby = "Meditation";
+
+//console.log(name + " " +age + " " + hobby);
+ console.log(`${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice`);
